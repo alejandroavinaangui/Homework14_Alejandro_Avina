ce14a1751aab6c5719bc7f4c35dd79d2ac39ffc1
alejandroavinaa <127714455+alejandroavinaa@users.noreply.github.com>
2025-11-14 12:59:05 -0800

    first change

--- Homework14_Alejandro_Avina/ContentView.swift
+++ Homework14_Alejandro_Avina/ContentView.swift
@@ -8,12 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var scale=1.0;
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Press twice to shrink")
+            
+            Image("rome")
+                .resizable()
+                .frame(width:500,height:400)
+                .scaleEffect(scale)
+                .onTapGesture(count:2){
+                    let minScale = scale - 0.5;
+                    if (minScale == 0.5){
+                        scale = minScale ;
+                    }
+                }
+                .onLongPressGesture(minimumDuration: 2) {
+                    let maxScale = scale + 0.5;
+                    scale = max(maxScale,0.5);
+                    }
+            
+            Text("Press down to to go back to normal")
         }
         .padding()
     }
